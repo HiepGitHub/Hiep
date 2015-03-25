7cf5c753a4b54997fbe04feee9d99c77af627283
Hiep Pham Duc <duchiep02th@gmail.com>
2015-03-25 12:25:10 +0700

    + add request getGroups by userID

--- LinkBoard/app/src/main/java/com/sss/linkboard/service/client/foldermenu/GetGroupByUserIDRequest.java
+++ LinkBoard/app/src/main/java/com/sss/linkboard/service/client/foldermenu/GetGroupByUserIDRequest.java
@@ -1,7 +1,42 @@
 package com.sss.linkboard.service.client.foldermenu;
 
-/**
- * Created by trongnhan on 3/25/15.
- */
-public class GetGroupByUserIDRequest {
+
+import android.content.Context;
+
+import com.android.volley.Response;
+import com.sss.linkboard.service.client.LinkBoardAPI;
+import com.sss.linkboard.service.network.GsonRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetGroupByUserIDRequest extends GsonRequest<GroupByUserIDItem>{
+
+    public static String TAG_REQUEST = GetGroupByUserIDRequest.class.getSimpleName();
+
+    /**
+     * Make a GET request and return a parsed object from JSON.
+     *
+     * @param method
+     * @param url           URL of the request to make
+     * @param clazz         Relevant class object, for Gson's reflection
+     * @param headers       Map of request headers
+     * @param params
+     * @param listener
+     * @param errorListener
+     */
+    public GetGroupByUserIDRequest(int method, String url, Class<GroupByUserIDItem> clazz, Map<String, String> headers, Map<String, String> params, Response.Listener<GroupByUserIDItem> listener, Response.ErrorListener errorListener) {
+        super(method, url, clazz, headers, params, listener, errorListener);
+    }
+    public GetGroupByUserIDRequest(Map<String, String> params, Response.Listener<GroupByUserIDItem> listener, Response.ErrorListener errorListener) {
+        this(Method.POST, LinkBoardAPI.GET_GROUP_BY_USER_ID, GroupByUserIDItem.class, headerUrlEncode(), params, listener, errorListener);
+        setTag(TAG_REQUEST);
+    }
+
+    public static Map<String, String> buildParam(Context context, String userID){
+        Map<String,String> params = new HashMap<String, String>();
+        params.put("userID", userID);
+        return params;
+    }
+
 }
---
+++ LinkBoard/app/src/main/java/com/sss/linkboard/service/client/foldermenu/GroupByUserIDItem.java
@@ -0,0 +1,40 @@
+package com.sss.linkboard.service.client.foldermenu;
+
+public class GroupByUserIDItem {
+    private String groupID;
+    private String userID;
+    private String title;
+    private String groupOrderID;
+
+    public String getGroupID() {
+        return groupID;
+    }
+
+    public void setGroupID(String groupID) {
+        this.groupID = groupID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGroupOrderID() {
+        return groupOrderID;
+    }
+
+    public void setGroupOrderID(String groupOrderID) {
+        this.groupOrderID = groupOrderID;
+    }
+}
