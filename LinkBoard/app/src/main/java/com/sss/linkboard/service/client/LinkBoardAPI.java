046255098fc635649803e3140ccdac91980a8b8d
Hiep Pham Duc <duchiep02th@gmail.com>
2015-03-25 10:02:06 +0700

    folder menu API

--- LinkBoard/app/src/main/java/com/sss/linkboard/service/client/LinkBoardAPI.java
+++ LinkBoard/app/src/main/java/com/sss/linkboard/service/client/LinkBoardAPI.java
@@ -9,4 +9,14 @@ public final class LinkBoardAPI {
 
     public static final String SIGN_UP_API = BASE_URL + "/users/register";
     public static final String LOGIN_API = BASE_URL + "/users/login";
+
+
+    //FOLDER MENU API
+
+    public static final String GET_GROUP_BY_USER_ID = BASE_URL + "/groups/getGroupsByUserID";
+    public static final String ADD_GROUP_BY_USER_ID_TITLE = BASE_URL + "/groups/addGroup";
+    public static final String EDIT_GROUP_BY_GROUP_ID_TITLE = BASE_URL + "/groups/editGroup";
+    public static final String REMOVE_GROUP_BY_GROUP_ID = BASE_URL + "/groups/removeGroup";
+    public static final String GET_FOLDER_BY_GROUP_ID = BASE_URL + "/folders/getFoldersByGroupID";
+    public static final String ADD_FOLDER_BY_GROUP_ID = BASE_URL + "/folders/addFolder";
 }
